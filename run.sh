20103686193d356729c1e1853854ff1d13657ba4
Yann Simon <simon@adesso.de>
2011-11-16 12:46:54 +0100

    do not set memory settings

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-../../play/play run --%prod -server -Xms1024m -Xmx1024m -XX:MaxPermSize=128m -XX:+OptimizeStringConcat -XX:-RelaxAccessControlCheck -XX:+UseParallelGC -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -XX:CompileThreshold=200 -XX:+CMSClassUnloadingEnabled
+../../play/play run --%prod -server -XX:+OptimizeStringConcat -XX:-RelaxAccessControlCheck -XX:+UseParallelGC -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -XX:CompileThreshold=200 -XX:+CMSClassUnloadingEnabled
